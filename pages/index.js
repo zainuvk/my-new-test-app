ac85f6ccf623f9b83a6062ff31c41b18a2e7df11
Zainuden Veetikadam <zainuvk@github.com>
2021-02-09 15:05:18 +0530

    changes to code

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Create new  App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
