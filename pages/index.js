b0644187a4229359af6c8c19f1d4fff23764f621
Zainuden Veetikadam <zainuvk@github.com>
2021-02-10 12:25:16 +0530

    testing by zainu

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to New App <a href="https://nextjs.org"> deployed</a>
+          New testing by Zainu <a href="https://nextjs.org"> deployed</a>
         </h1>
 
         <p className={styles.description}>
